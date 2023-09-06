3ccadc51067ffec53f74ded6aa39b5fb23394431
dfd <kchoverwatch3@gmail.com>
2023-09-06 15:53:36 +0900

    11일차

--- Assets/Scripts/UI/UI_Button.cs
+++ Assets/Scripts/UI/UI_Button.cs
@@ -3,16 +3,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using Object = UnityEngine.Object;
 
 public class UI_Button : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI _text;
+    private Dictionary<Type, UnityEngine.Object[]> _objects = new Dictionary<Type, Object[]>();
+
+    enum Buttons
+    {
+        PointButton
+    }
+
+    enum Texts
+    {
+        PointText,
+        ScoreText
+    }
+
+    void Bind()
+    {
+        
+    }
+
+    private void Start()
+    {
+        Bind<Button>(typeof(Buttons));
+        Bind<TextMeshProUGUI>(typeof(Texts));
+    }
+
+    void Bind<T>(Type type) where T : UnityEngine.Object
+    {
+        string[] names = Enum.GetNames(type);
+
+        UnityEngine.Object[] objects = new UnityEngine.Object[names.Length];
+        _objects.Add(typeof(T), objects);
+
+        for (int i = 0; i < names.Length; i++)
+        {
+            objects[i] = Util.FindChild<T>(gameObject, names[i], true);
+        }
+    }
+
     private int _score = 0;
     
     public void OnButtonClicked()
     {
         _score++;
         Debug.Log("Btn clicked");
-        _text.text = $"점수 : {_score}";
+        //_text.text = $"점수 : {_score}";
     }
 }
